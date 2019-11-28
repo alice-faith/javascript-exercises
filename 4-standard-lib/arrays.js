95a5fe4091114d1ce0edbc3113695e3c45e48109
alice-faith <alicfaith18@gmail.com>
2019-11-28 11:30:58 +0400

    example 11

--- 4-standard-lib/arrays.js
+++ 4-standard-lib/arrays.js
@@ -199,7 +199,10 @@ describe("arrays", function() {
 
             ////////////////////////////////////////////////////////////////////
             // ✏️ ADD CODE HERE. USE THE STANDARD LIB, NO LOOPS! ✏️
-
+            
+            function sortByLength() {
+                return array.sort((a,b ) => a.length - b.length);
+            }
             ////////////////////////////////////////////////////////////////////
 
             expect(sortByLength()).to.equal(array);
